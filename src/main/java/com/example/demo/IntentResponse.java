1b38c9b57b51405d482d94a98addcaf09083abf8
Alok <alook.gupta@gmail.com>
2019-04-25 11:57:14 +0530

    testing

--- src/main/java/com/example/demo/IntentResponse.java
+++ src/main/java/com/example/demo/IntentResponse.java
@@ -16,28 +16,28 @@ class DiagnosticInfo {
 
 }
 
-class Intent {
-
-	private String name;
-	private String displayName;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDisplayName() {
-		return displayName;
-	}
-
-	public void setDisplayName(String displayName) {
-		this.displayName = displayName;
-	}
-
-}
+//class Intent {
+//
+//	private String name;
+//	private String displayName;
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getDisplayName() {
+//		return displayName;
+//	}
+//
+//	public void setDisplayName(String displayName) {
+//		this.displayName = displayName;
+//	}
+//
+//}
 
 public class IntentResponse {
 
@@ -80,118 +80,118 @@ public class IntentResponse {
 
 }
 
-class OutputContext {
-
-	private String name;
-	private Integer lifespanCount;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getLifespanCount() {
-		return lifespanCount;
-	}
-
-	public void setLifespanCount(Integer lifespanCount) {
-		this.lifespanCount = lifespanCount;
-	}
-
-}
-
-class Parameters {
-
-}
-
-class QueryResult {
-
-	private String queryText;
-	private String action;
-	private Parameters parameters;
-	private Boolean allRequiredParamsPresent;
-	private List<OutputContext> outputContexts = null;
-	private Intent intent;
-	private Integer intentDetectionConfidence;
-	private DiagnosticInfo diagnosticInfo;
-	private String languageCode;
-
-	public String getQueryText() {
-		return queryText;
-	}
-
-	public void setQueryText(String queryText) {
-		this.queryText = queryText;
-	}
-
-	public String getAction() {
-		return action;
-	}
-
-	public void setAction(String action) {
-		this.action = action;
-	}
-
-	public Parameters getParameters() {
-		return parameters;
-	}
-
-	public void setParameters(Parameters parameters) {
-		this.parameters = parameters;
-	}
-
-	public Boolean getAllRequiredParamsPresent() {
-		return allRequiredParamsPresent;
-	}
-
-	public void setAllRequiredParamsPresent(Boolean allRequiredParamsPresent) {
-		this.allRequiredParamsPresent = allRequiredParamsPresent;
-	}
-
-	public List<OutputContext> getOutputContexts() {
-		return outputContexts;
-	}
-
-	public void setOutputContexts(List<OutputContext> outputContexts) {
-		this.outputContexts = outputContexts;
-	}
-
-	public Intent getIntent() {
-		return intent;
-	}
-
-	public void setIntent(Intent intent) {
-		this.intent = intent;
-	}
-
-	public Integer getIntentDetectionConfidence() {
-		return intentDetectionConfidence;
-	}
-
-	public void setIntentDetectionConfidence(Integer intentDetectionConfidence) {
-		this.intentDetectionConfidence = intentDetectionConfidence;
-	}
-
-	public DiagnosticInfo getDiagnosticInfo() {
-		return diagnosticInfo;
-	}
-
-	public void setDiagnosticInfo(DiagnosticInfo diagnosticInfo) {
-		this.diagnosticInfo = diagnosticInfo;
-	}
-
-	public String getLanguageCode() {
-		return languageCode;
-	}
-
-	public void setLanguageCode(String languageCode) {
-		this.languageCode = languageCode;
-	}
-
-}
+//class OutputContext {
+//
+//	private String name;
+//	private Integer lifespanCount;
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public Integer getLifespanCount() {
+//		return lifespanCount;
+//	}
+//
+//	public void setLifespanCount(Integer lifespanCount) {
+//		this.lifespanCount = lifespanCount;
+//	}
+//
+//}
+//
+//class Parameters {
+//
+//}
+//
+//class QueryResult {
+//
+//	private String queryText;
+//	private String action;
+//	private Parameters parameters;
+//	private Boolean allRequiredParamsPresent;
+//	private List<OutputContext> outputContexts = null;
+//	private Intent intent;
+//	private Integer intentDetectionConfidence;
+//	private DiagnosticInfo diagnosticInfo;
+//	private String languageCode;
+//
+//	public String getQueryText() {
+//		return queryText;
+//	}
+//
+//	public void setQueryText(String queryText) {
+//		this.queryText = queryText;
+//	}
+//
+//	public String getAction() {
+//		return action;
+//	}
+//
+//	public void setAction(String action) {
+//		this.action = action;
+//	}
+//
+//	public Parameters getParameters() {
+//		return parameters;
+//	}
+//
+//	public void setParameters(Parameters parameters) {
+//		this.parameters = parameters;
+//	}
+//
+//	public Boolean getAllRequiredParamsPresent() {
+//		return allRequiredParamsPresent;
+//	}
+//
+//	public void setAllRequiredParamsPresent(Boolean allRequiredParamsPresent) {
+//		this.allRequiredParamsPresent = allRequiredParamsPresent;
+//	}
+//
+//	public List<OutputContext> getOutputContexts() {
+//		return outputContexts;
+//	}
+//
+//	public void setOutputContexts(List<OutputContext> outputContexts) {
+//		this.outputContexts = outputContexts;
+//	}
+//
+//	public Intent getIntent() {
+//		return intent;
+//	}
+//
+//	public void setIntent(Intent intent) {
+//		this.intent = intent;
+//	}
+//
+//	public Integer getIntentDetectionConfidence() {
+//		return intentDetectionConfidence;
+//	}
+//
+//	public void setIntentDetectionConfidence(Integer intentDetectionConfidence) {
+//		this.intentDetectionConfidence = intentDetectionConfidence;
+//	}
+//
+//	public DiagnosticInfo getDiagnosticInfo() {
+//		return diagnosticInfo;
+//	}
+//
+//	public void setDiagnosticInfo(DiagnosticInfo diagnosticInfo) {
+//		this.diagnosticInfo = diagnosticInfo;
+//	}
+//
+//	public String getLanguageCode() {
+//		return languageCode;
+//	}
+//
+//	public void setLanguageCode(String languageCode) {
+//		this.languageCode = languageCode;
+//	}
+//
+//}
 
 class WebhookStatus {
 
